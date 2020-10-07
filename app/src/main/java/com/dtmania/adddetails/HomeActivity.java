1e12c668dac495cdc3359a2fc313f3403a78889a
Dhaval-Thummar-529 <190304105062@paruluniversity.ac.in>
2020-10-07 15:11:18 +0530

    AD first Commmit

---
+++ app/src/main/java/com/dtmania/adddetails/HomeActivity.java
@@ -0,0 +1,44 @@
+package com.dtmania.adddetails;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+
+public class HomeActivity extends AppCompatActivity {
+    private Button button;
+    private EditText name, email, profession;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        name = findViewById(R.id.editName);
+        email = findViewById(R.id.editEmailAddDetails);
+        profession = findViewById(R.id.editProfession);
+        button = findViewById(R.id.addDetails);
+        Intent intent = getIntent();
+        email.setText(intent.getStringExtra("email"));
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (name.getText().toString().isEmpty() || email.getText().toString().isEmpty() || profession.getText().toString().isEmpty()) {
+                    Toast.makeText(HomeActivity.this, "Please fill all the details!", Toast.LENGTH_SHORT).show();
+                } else {
+                    HashMap<String, Object> map = new HashMap<>();
+                    map.put("Name", name.getText().toString());
+                    map.put("Profession", profession.getText().toString());
+                    map.put("Email", email.getText().toString());
+                    FirebaseDatabase.getInstance().getReference().push().updateChildren(map);
+                }
+            }
+        });
+    }
+}
